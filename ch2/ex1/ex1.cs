27fc9cfc0c8706797da4b9e572b72fab977b2050
mike-b88 <leeosam2017@protonmail.com>
2019-03-26 23:30:07 -0700

    First example completed with NotePad++, will start using VS2017 projects soon

---
+++ ch2/ex1/ex1.cs
@@ -0,0 +1,18 @@
+//Ch02_Basics
+
+using System;
+
+namespace Ch2_Basics
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Consoles.WriteLine("Hello World!");
+		}
+	}
+}
+
+//Written on NotePad++, haven't compiled or created on VS2017....
+// I will have to start creating VS project folders and pushing them onto github.
+// Or just use mono
